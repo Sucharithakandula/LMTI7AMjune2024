781e90c1be689ee5d1ef7edac122962ac5eafeb4
Sucharithakandula <kandulasucharitha3@gmail.com>
2024-06-15 17:15:04 +0530

    adding fluent wait in explicit code

--- RefactoMay2024Project/src/main/java/pageobjects/LearnMoreExplicitPage.java
+++ RefactoMay2024Project/src/main/java/pageobjects/LearnMoreExplicitPage.java
@@ -1,6 +1,7 @@
 package pageobjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,7 +22,9 @@ public class LearnMoreExplicitPage {
     }
 
     public String getTextChange(){
-        WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(12));
+        WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driverWait.pollingEvery(Duration.ofSeconds(5));
+        driverWait.ignoring(NoSuchElementException.class);
         driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[text()='Hello, Learn More Aspirants']")));
        return driver.findElement(By.xpath("//h3[text()='Hello, Learn More Aspirants']")).getText();
 
